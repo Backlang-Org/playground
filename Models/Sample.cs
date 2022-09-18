50f08a4bb9481cff86621a49f31abadeb1bb222a
filmee24 <filmee24@gmail.com>
2022-09-18 11:08:52 +0200

    Add Sample Loading

---
+++ Models/Sample.cs
@@ -0,0 +1,8 @@
+﻿namespace playground.Models
+{
+    public class Sample
+    {
+        public string Name { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ SampleLoader.cs
@@ -0,0 +1,22 @@
+﻿using playground.Models;
+using System.Net.Http.Json;
+
+namespace playground
+{
+    public class SampleLoader
+    {
+        public SampleLoader(HttpClient client)
+        {
+            HttpClient = client;
+        }
+
+        public HttpClient HttpClient { get; set; }
+
+        public async Task<string> GetSampleAsync(string id)
+        {
+            var sample = await HttpClient.GetFromJsonAsync<Sample>($"/samples/{id}.json");
+
+            return sample.Body;
+        }
+    }
+}
